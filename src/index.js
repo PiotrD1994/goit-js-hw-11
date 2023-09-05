1f4f1ebb6c90f3c695f10df0049c637c1b370764
Piotr Dubiel <piotrdubiel30@gmail.com>
2023-09-05 22:12:17 +0200

    hw 11

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import { fetchImages } from './fetch'
 
 const button = document.querySelector('[type="submit"]')
 const inputField = document.querySelector('[name="searchQuery"]')
+const form = document.querySelector('.search-form')
 
+form.addEventListener('submit', chosenGallery)
 
-button.addEventListener('click', chosenGallery)
-
-function chosenGallery () {
+function chosenGallery (event) {
+    event.preventDefault()
     const inputValue = inputField.value
     fetchImages(inputValue).then(data => {
         console.log(data)
